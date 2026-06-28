72022afeabd4e14d9d81d9faf2b01b43718197e0
Daniel Henrique <daniboyoliver2006@gmail.com>
2026-06-28 00:54:46 -0300

    Adição de botões interativos

--- src/components/ContentHome.tsx
+++ src/components/ContentHome.tsx
@@ -11,12 +11,13 @@ import git from '../assets/img/git-original.svg';
 export function ContentHome() {
     return (
         <main>
-            <section className="p-5 d-flex justify-content-center conteudo m-3 border rounded-5">
+            <section className="p-5 d-flex justify-content-center conteudo m-3 rounded-5">
                 <div className="container">
                     <div className="row row-cols-1 row-cols-md-2 g-4">
                         <div className="col">
-                            <h1 className="display-4 titulo p-2 mb-0">Bem-vindo ao meu portfólio!</h1>
-                            <p className="p-2 texto-descricao">Eu sou Daniel Henrique Leão de Oliveira, tenho 19 anos e sou um desenvolvedor full-stack apaixonado por criar soluções inovadoras e experiências digitais memoráveis. <i><br/> Meu Stack logo abaixo:</i></p>
+                            <h1 className="display-4 titulo p-2 mb-0 pb-0">Bem-vindo ao meu portfólio!</h1>
+                            <p className="p-2 texto-descricao">Desenvolvedor Full Stack com ênfase em Front-End </p>
+                            <i><h5>Principais Tecnologias:</h5></i>
                             <img src={bs} className="img-fluid rounded-5 icone-tech p-1" alt="Imagem de exemplo" />
                             <img src={css3} className="img-fluid rounded-5 icone-tech p-1" alt="Imagem de exemplo" />
                             <img src={html5} className="img-fluid rounded-5 icone-tech p-1" alt="Imagem de exemplo" />
@@ -30,8 +31,25 @@ export function ContentHome() {
                             <img src={close} className="img-fluid rounded-5 perfil" alt="Imagem de exemplo" />
                         </div>
                     </div>
-                    <div className="row"></div>
                 </div>
+            </section>
+            <section className="conteudo rounded-5 p-5 d-flex justify-content-center m-3">
+                <div className="container d-flex justify-content-center">
+                    <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 d-flex justify-content-center">
+                        <div className="col d-flex my-auto">
+                            <a href="#" className="btn github mx-auto my-auto">Meu Github
+                                <img src={github} className="img-fluid rounded-5 icone-btn m-1" alt="Imagem de exemplo" />
+                            </a>
+                        </div>
+                        <div className="col my-auto">
+                            <a href="#" className="btn mx-auto my-auto">Meu LinkedIn</a>
+                        </div>
+                        <div className="col my-auto">
+                            <a href="#" className="btn mx-auto my-auto">Baixe meu Currículo</a>
+                        </div>
+                    </div>
+                </div>
+
             </section>
         </main>
     )
