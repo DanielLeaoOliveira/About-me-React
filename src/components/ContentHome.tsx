5face5c8468b2d95a3cdc727b0ba6d7aaab2ac51
Daniel Henrique <daniboyoliver2006@gmail.com>
2026-06-29 20:58:19 -0300

    Fix: Responsividade em resolução de celular

--- src/components/ContentHome.tsx
+++ src/components/ContentHome.tsx
@@ -18,8 +18,11 @@ export function ContentHome() {
                 <div className="container">
                     <div className="row row-cols-1 row-cols-md-2 g-4">
                         <div className="col">
-                            <h1 className="display-4 titulo p-2 mb-0 pb-0">Bem-vindo ao meu portfólio!</h1>
-                            <p className="p-2 texto-descricao">Desenvolvedor Full Stack com ênfase em Front-End </p>
+                            <h1 className="display-4 titulo mt-4 mb-0 pb-0">Bem-vindo ao meu portfólio!</h1>
+                            <p className="texto-descricao">Desenvolvedor Full Stack com ênfase em Front-End <br />
+                            <i>Criando interfaces modernas e responsivas.</i></p>
+                            <hr />
+                            
                             <i><h5>Principais Tecnologias:</h5></i>
                             <img src={bs} className="img-fluid rounded-5 icone-tech p-1" alt="Imagem de exemplo" />
                             <img src={css3} className="img-fluid rounded-5 icone-tech p-1" alt="Imagem de exemplo" />
@@ -31,7 +34,7 @@ export function ContentHome() {
                             <img src={git} className="img-fluid rounded-5 icone-tech p-1" alt="Imagem de exemplo" />
                         </div>
                         <div className="col">
-                            <img src={close} className="img-fluid rounded-5 perfil" alt="Imagem de exemplo" />
+                            <img src={close} className="img-fluid rounded-5 perfil mt-4" alt="Imagem de exemplo" />
                         </div>
                     </div>
                 </div>
@@ -39,22 +42,21 @@ export function ContentHome() {
             <section className="conteudo rounded-5 p-5 d-flex justify-content-center m-3">
                 <div className="container d-flex justify-content-center">
                     <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 d-flex justify-content-center">
-                        <div className="col d-flex my-auto">
+                        <div className="col d-flex my-auto mt-2">
                             <Link to="https://github.com/DanielLeaoOliveira" className="btn github mx-auto my-auto" target="_blank">Meu Github
                                 <img src={github} className="img-fluid icone-btn m-1" alt="Imagem de exemplo" />
                             </Link>
                         </div>
-                        <div className="col my-auto">
-                            <Link to="www.linkedin.com/in/daniel-henrique-leão-de-oliveira-6020a0369" className="btn linkedin mx-auto my-auto p-2" target="_blank">Meu LinkedIn
+                        <div className="col d-flex my-auto mt-2">
+                            <Link to="www.linkedin.com/in/daniel-henrique-leão-de-oliveira-6020a0369" className="btn linkedin mx-auto my-auto" target="_blank">Meu LinkedIn
                                 <img src={linkedin} className="img-fluid icone-btn lnkdn m-1" alt="Imagem de exemplo" />
                             </Link>
                         </div>
-                        <div className="col my-auto">
+                        <div className="col d-flex my-auto mt-2">
                             <Link to="https://drive.google.com/uc?export=download&id=1AKwmuWjfERZjO3Qk2_b6gx-pW8c0XR8C" className="btn curriculo mx-auto my-auto" target="_blank">Baixe meu Currículo</Link>
                         </div>
                     </div>
                 </div>
-
             </section>
         </main>
     )
