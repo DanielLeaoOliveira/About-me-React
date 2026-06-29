135bebf39566d63555e765da7d52b2a311bbb7ed
Daniel Henrique <daniboyoliver2006@gmail.com>
2026-06-28 21:09:35 -0300

    Botões com links funcionais e Link formatado para React-router-dom

--- src/components/ContentHome.tsx
+++ src/components/ContentHome.tsx
@@ -7,6 +7,9 @@ import react from '../assets/img/react-original.svg';
 import reactbootstrap from '../assets/img/reactbootstrap.svg';
 import github from '../assets/img/github-original.svg';
 import git from '../assets/img/git-original.svg';
+import linkedin from '../assets/img/linkedin-original.svg';
+import { Link } from 'react-router-dom';
+
 
 export function ContentHome() {
     return (
@@ -37,15 +40,17 @@ export function ContentHome() {
                 <div className="container d-flex justify-content-center">
                     <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 d-flex justify-content-center">
                         <div className="col d-flex my-auto">
-                            <a href="#" className="btn github mx-auto my-auto">Meu Github
-                                <img src={github} className="img-fluid rounded-5 icone-btn m-1" alt="Imagem de exemplo" />
-                            </a>
+                            <Link to="https://github.com/DanielLeaoOliveira" className="btn github mx-auto my-auto" target="_blank">Meu Github
+                                <img src={github} className="img-fluid icone-btn m-1" alt="Imagem de exemplo" />
+                            </Link>
                         </div>
                         <div className="col my-auto">
-                            <a href="#" className="btn mx-auto my-auto">Meu LinkedIn</a>
+                            <Link to="www.linkedin.com/in/daniel-henrique-leão-de-oliveira-6020a0369" className="btn linkedin mx-auto my-auto p-2" target="_blank">Meu LinkedIn
+                                <img src={linkedin} className="img-fluid icone-btn lnkdn m-1" alt="Imagem de exemplo" />
+                            </Link>
                         </div>
                         <div className="col my-auto">
-                            <a href="#" className="btn mx-auto my-auto">Baixe meu Currículo</a>
+                            <Link to="https://drive.google.com/uc?export=download&id=1AKwmuWjfERZjO3Qk2_b6gx-pW8c0XR8C" className="btn curriculo mx-auto my-auto" target="_blank">Baixe meu Currículo</Link>
                         </div>
                     </div>
                 </div>
